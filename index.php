d6f795ffe882343628735af00baf6273f008844d
Theobasket29 <blancotheo29@gmail.com>
2025-01-09 22:52:11 +0100

    déplacement des fichier

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokémon</title>
+    <link rel="stylesheet" href="./CSS/index.css">
+</head>
+<?php include './Front/header.html'; ?>
+<body>
+</body>
+</html>
+
