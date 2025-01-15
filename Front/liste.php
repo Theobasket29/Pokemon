55b218bc09f138c8d1601cbd55c67102408c0f37
Theobasket29 <blancotheo29@gmail.com>
2025-01-15 01:35:19 +0100

    Suppresion connexion
    Supression de la connexion dans les fichier php

--- Front/liste.php
+++ Front/liste.php
@@ -1,16 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pokemon";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include '../Back/connexion.php';
 
 // Retrieve data from the database
 $sql = "SELECT nom, complement, pv, type, collection, annee, langue, pays, rarete, prix, numero, evolution FROM pokemon";
